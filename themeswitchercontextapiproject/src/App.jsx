b59d30c81979b717ad4877c09863388e5e23f812
KaranKhulbe <karankhulbe24@gmail.com>
2024-09-06 03:07:52 +0530

    addedcontextinthemeproject

--- themeswitchercontextapiproject/src/App.jsx
+++ themeswitchercontextapiproject/src/App.jsx
@@ -1,11 +1,37 @@
-
+import React, { useEffect } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
 
 function App() {
+  const [themeMode, setthemeMode] = usestate("light")
+
+  const lightTheme =()=>{
+    setthemeMode("light")
+  }
+  const darkTheme=()=>{
+    setthemeMode("dark")
+  }
+  //actual change in the theme
+  useEffect(()=>{
+    document.querySelector('html').classList.remove("light","dark")
+    document.querySelector('html').classList.add(themeMode)
+
+  },[themeMode])
 
   return (
     <>
-    <h1 className='p-4 bg-pink-600 text-3xl'> Theme Switcher Project</h1>
+<ThemeProvider value={{themeMode,lightTheme, darkTheme}}> 
+<div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                      { /* themeBtn */}
+            </div>
+             <div className="w-full max-w-sm mx-auto">
+               {/* Card */}
+              </div>
+              </div>
+            </div>
+</ThemeProvider>
     </>
   )
 }
