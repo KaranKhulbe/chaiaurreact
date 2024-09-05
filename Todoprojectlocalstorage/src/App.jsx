70ec49b7851b5231990b4873c9242ba86c87e0fa
KaranKhulbe <karankhulbe24@gmail.com>
2024-09-06 04:16:30 +0530

    Create Todo project

---
+++ Todoprojectlocalstorage/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  <>
+  <h1 className='text-3xl'> Today is a Productive Day</h1>
+    </>
+  
+}
+
+export default App
