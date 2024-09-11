7d2f5053ea2001da4565024ef079592d1ef13986
KaranKhulbe <karankhulbe24@gmail.com>
2024-09-12 01:46:16 +0530

    Completed adding all pages

--- Blog application appwrite backend/src/pages/AllPosts.jsx
+++ Blog application appwrite backend/src/pages/AllPosts.jsx
@@ -14,9 +14,14 @@ function AllPosts() {
   return (
     <div className='w-full py-8'>
         <Container>
-            {posts.map((post)=>{
-                <PostCard key={post.$id} post={post}/>
-            })}
+            <div className='flex flex-wrap'>
+                {posts.map((post)=>(
+                    <div key={post.$id}
+                    className='p-2 w-1/4'>
+                        <PostCard post={post}/>
+                    </div>
+                ))}
+            </div>
         </Container>
     </div>
   )
